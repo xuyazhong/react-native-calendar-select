627481ea578eee400cc00ed9c6a0b9151943b967
xuyazhong <yazhongxu@gmail.com>
2019-02-22 15:25:20 +0800

    update

--- Calendar.js
+++ Calendar.js
@@ -4,23 +4,27 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import {NativeModules} from 'react-native';
 
 let datePicker = NativeModules.CalenderModule;
 
-export default class Calendar extends Component {
+class Calendar {
 
     dp_normal(date = '', callback) {
+        console.log("dp_normal")
       datePicker.selectNormalDate(date, callback)
     }
 
     dp_past(date = '', callback) {
+        console.log("dp_past")
         datePicker.selectPastDatePicker(date, callback)
     }
 
     dp_feature(date = '', callback) {
+        console.log("dp_feature")
         datePicker.selectFeatureDatePicker(date, callback)
     }
 
 }
+
+module.exports = new Calendar();
